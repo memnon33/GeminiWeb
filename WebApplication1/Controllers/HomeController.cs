fd13366e8a35329c3e7dcb39df68b14662a16601
Jason Farmer <jason@memnon.co.uk>
2016-07-20 20:30:35 +0100

    Initial Code commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Gemini Cheer";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Gemini Cheer contact page.";
+
+            return View();
+        }
+        public ActionResult MiniGem()
+        {
+            ViewBag.Message = "Gemini minigems.";
+
+            return View();
+        }
+        public ActionResult JuniorA()
+        {
+            ViewBag.Message = "Gemini Junior As.";
+
+            return View();
+        }
+        public ActionResult JuniorB()
+        {
+            ViewBag.Message = "Gemini Junior Bs.";
+
+            return View();
+        }
+        public ActionResult Seniors()
+        {
+            ViewBag.Message = "Gemini Seniors";
+
+            return View();
+        }
+        public ActionResult Masters()
+        {
+            ViewBag.Message = "Gemini Masters";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/NewsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /News/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "News index";
+            return View();
+        }
+
+        public ActionResult News()
+        {
+            ViewBag.Message = "News page";
+            return View();
+        }
+	}
+}
